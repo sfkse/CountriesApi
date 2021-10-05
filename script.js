a369784105a7b755833c9b4c3f15ced0e3978142
Sefa Kose <sfkse5591@gmail.com>
2021-10-05 20:42:05 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+    console.log(Object.keys(data.currencies)[0])
+    const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.svg}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[Object.keys(data.languages)[0]]}</p>
+        <p class="country__row"><span>💰</span>${Object.keys(data.currencies)[0]}</p>
+      </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            renderCountry(data[0]);
+            const neighbour = data[0].borders[0].toLowerCase();
+
+            if (!neighbour) return;
+
+            return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
+        })
+        .then(response => response.json())
+        .then(data => renderCountry(data[0], "neighbour"))
+}
+
+getCountryData('turkey')
+
